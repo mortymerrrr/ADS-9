382cdf04b4b26c1efa5b03e05a00fa1b22147f1d
mortymerrrr <160071899+mortymerrrr@users.noreply.github.com>
2024-05-25 22:04:26 +0300

    b58.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,22 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> bstTree;
+  std::ifstream file(filename);
+  std::string word;
+  while (!file.eof()) {
+    char character = file.get();
+    if ((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z')) {
+      word += std::tolower(character);
+    } else if (!word.empty()) {
+      bstTree.add(word);
+      word.clear();
+    }
+  }
+  return bstTree;
 }
